75f0ae8637d41aec2a610e22a2ff58facdd49238
Tae-Yoon Kim <161553878+kty2001@users.noreply.github.com>
2026-07-01 16:15:00 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.54 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,15 @@
+def solution(board, moves):
+    answer = 0
+    stack = []
+    n = len(board)
+    for m in moves:
+        for i in range(n):
+            if board[i][m-1] != 0:
+                if stack and stack[-1] == board[i][m-1]:
+                    answer += 1
+                    stack.pop()
+                else:
+                    stack.append(board[i][m-1])
+                board[i][m-1] = 0
+                break
+    return answer*2
