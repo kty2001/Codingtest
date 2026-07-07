d925351626a306506a9514a14fc55139e3f113d2
Tae-Yoon Kim <161553878+kty2001@users.noreply.github.com>
2026-07-07 15:36:41 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.02 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -0,0 +1,27 @@
+def solution(video_len, pos, op_start, op_end, commands):
+    answer = ''
+    video_len = int(video_len.replace(':', ''))
+    pos = int(pos.replace(':', ''))
+    op_start = int(op_start.replace(':', ''))
+    op_end = int(op_end.replace(':', ''))
+    
+    if op_start <= pos < op_end:
+        pos = op_end
+    
+    for c in commands:
+        if op_start <= pos < op_end:
+            pos = op_end
+        if c == 'next':
+            if pos%100 >= 50:
+                pos = (pos//100 + 1)*100 + (pos%100 - 60)
+            pos = min(video_len, pos+10)
+        elif c == 'prev':
+            if pos%100 < 10:
+                pos = (pos//100 - 1)*100 + (pos%100 + 60)
+            pos = max(0, pos-10)
+            
+    if op_start <= pos < op_end:
+        pos = op_end
+    
+    pos = str(pos).zfill(4)
+    return pos[:2]+':'+pos[2:]
