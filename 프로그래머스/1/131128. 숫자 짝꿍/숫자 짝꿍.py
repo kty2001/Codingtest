bb0bcba1a37f8b40f7708983378b63c58d00a58f
Tae-Yoon Kim <161553878+kty2001@users.noreply.github.com>
2026-07-07 19:05:08 +0900

    [level 1] Title: 숫자 짝꿍, Time: 85.19 ms, Memory: 28.9 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,9 +1,6 @@
 def solution(X, Y):
     dig = {str(i): min(X.count(str(i)), Y.count(str(i))) for i in range(10)}
-    print(dig)
     answer = ''.join(k*v for k, v in dig.items())[::-1]
-    if not answer:
-        return '-1'
-    if len(answer) == answer.count('0'):
-        answer = '0'
+    if not answer: return '-1'
+    if len(answer) == answer.count('0'): return '0'
     return answer
