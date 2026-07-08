9f70d699e3c5f891e0cda9c008367258f93e7b1d
Tae-Yoon Kim <161553878+kty2001@users.noreply.github.com>
2026-07-08 15:12:37 +0900

    [level 1] Title: 노란불 신호등, Time: 227.49 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -1,9 +1,8 @@
 def solution(signals):
+    import math
     sig = ['G'*s[0] + 'Y'*s[1] + 'R'*s[2] for s in signals]
-    i_max = 1
-    for s in signals:
-        i_max *= sum(s)
+    i_max = math.lcm(*(sum(s) for s in signals))
     
     for i in range(i_max):
-        if {s[i%len(s)] for s in sig} == {'Y'}: return i + 1
+        if all(s[i%len(s)] == 'Y' for s in sig): return i + 1
     return -1
