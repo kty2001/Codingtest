96828569f8d8b5b63e88ccf447d3502aa7a09a2f
Tae-Yoon Kim <161553878+kty2001@users.noreply.github.com>
2026-07-09 11:02:37 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -2,6 +2,4 @@ def solution(n):
     i = 1
     while bin(n+i)[2:].count('1') != bin(n)[2:].count('1'):
         i += 1
-    while bin(n+i)[2:].count('1') != bin(n)[2:].count('1'):
-        i += 1
     return n+i
