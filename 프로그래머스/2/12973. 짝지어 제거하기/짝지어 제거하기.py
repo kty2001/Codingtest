2fc2fe8c23809a23bebd73d9486b98b9f1ed33fa
Tae-Yoon Kim <161553878+kty2001@users.noreply.github.com>
2026-07-09 13:39:53 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.19 ms, Memory: 13.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in s:
+        if stack and stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if stack: return 0
+    else: return 1
