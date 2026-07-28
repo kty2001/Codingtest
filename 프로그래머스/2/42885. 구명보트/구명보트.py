8823d0f264f379cd455a8345dfadf9d997f3f3c7
Tae-Yoon Kim <161553878+kty2001@users.noreply.github.com>
2026-07-28 09:17:20 +0900

    [level 2] Title: 구명보트, Time: 7.65 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,14 +1,9 @@
 def solution(people, limit):
     answer = 0
-    boat = 0
     front, rear = 0, len(people) - 1
     people = sorted(people, reverse=True)
-    while front <= rear:
-        if front == rear:
-            return answer + 1
-        
-        boat = people[front]
-        if boat + people[rear] <= limit:
+    while front <= rear:        
+        if people[front] + people[rear] <= limit:
             rear -= 1
         front += 1
         answer += 1
